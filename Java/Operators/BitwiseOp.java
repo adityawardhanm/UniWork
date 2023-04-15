39fb3d18499d08a289d32499bde9c9e9a97b26ac
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-15 09:08:05 -0700

    Create BitwiseOp.java

---
+++ Java/Operators/BitwiseOp.java
@@ -0,0 +1,9 @@
+public class BitwiseOp {
+    public static void main(String args[]){
+        //Following is an Example of Bitwise Operator
+        int a=2, b=3, c=4;
+        System.out.println("a<b^a<c is " + (a<b^a<c));
+        System.out.println("a<b|a<c is " + (a<b|a<c));
+        System.out.println("a<b&a<c is " + (a<b&a<c));
+    }
+}
