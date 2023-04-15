afa129f407cf92c842a1dbe3e2b8434d77a9acaf
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-15 09:19:13 -0700

    Create TernaryOp.java

---
+++ Java/Operators/TernaryOp.java
@@ -0,0 +1,8 @@
+public class TernaryOp {
+    public static void main(String args[]){
+        int a=2, b=3;
+        //Following is an Example of Ternary Operator
+        System.out.println("a<b)?a:b is " + ((a<b)?a:b));
+        System.out.println("a>b)?a:b is " + ((a>b)?a:b));
+    }
+}
