c1b46bfd46e2684eaf7389812b567068c2af3c59
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-15 07:25:38 -0700

    Create RelationalOp.java

---
+++ Java/RelationalOp.java
@@ -0,0 +1,11 @@
+public class RelationalOp {
+        public static void main(String[] args) {
+            int a = 2, b = 3;
+            System.out.println("a == b is " + (a == b));
+            System.out.println("a != b is " + (a != b));
+            System.out.println("a >  b is " + (a > b));
+            System.out.println("a >= b is " + (a >= b));
+            System.out.println("a <  b is " + (a < b));
+            System.out.println("a <= b is " + (a <= b));
+        }
+}
