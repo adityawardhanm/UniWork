07b897ee768ffdf4770bb5c177a2f1f6d27289d6
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-15 07:49:04 -0700

    Update RelationalOp.java

--- Java/Operators/RelationalOp.java
+++ Java/Operators/RelationalOp.java
@@ -1,5 +1,6 @@
 public class RelationalOp {
         public static void main(String[] args) {
+            //Following is an Example of Arithmetic Operator
             int a = 2, b = 3;
             System.out.println("a == b is " + (a == b));
             System.out.println("a != b is " + (a != b));
