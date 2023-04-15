83503e5046603a28b3e37ac560621264f94ee468
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-15 07:29:18 -0700

    Create UnaryOp.java

---
+++ Java/Operators/UnaryOp.java
@@ -0,0 +1,10 @@
+public class UnaryOp {
+    public static void main(String[] args) {
+        //Following is an Example of Unary Operator
+        int a = 2;
+        System.out.println("a++  is " + (a++));//a=2(3)
+        System.out.println("++a  is " + (++a));//a=4
+        System.out.println("a-- is " + (a--));//a=4(3)
+        System.out.println("--a  is " + (--a));//a=2
+    }
+}
