87b55d37c9dcb2a17475f0395e5fb63f860123d4
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-18 18:46:12 -0700

    Create inheritance.java

---
+++ Java/Inheritance/inheritance.java
@@ -0,0 +1,16 @@
+class dp extends inheritance{
+    void department(){
+        System.out.println("USCI");
+        System.out.println("UID");
+        System.out.println("KSD");
+        System.out.println("USLM");
+        System.out.println("UWSL");
+    }
+}
+public class inheritance {
+    public static void main(String[] args) {
+        dp dpt = new dp();
+        System.out.println("Department: ");
+        dpt.department();
+    }
+}
