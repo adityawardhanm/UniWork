a2667aef674696d61283d8b47b92ebd7bb0b1725
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-04-21 07:55:10 -0700

    Create for_loop.java

---
+++ Java/Loops-Control-Statements/for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args) {
+        System.out.println("For Loop");
+        for (int i=0; i<5; i++){
+            for (int j=0; j<=i;j++){System.out.print("* ");}
+            System.out.println("");
+        }
+    }
+}
