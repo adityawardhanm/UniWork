3fa55abf709dd4ad5ae5725ba7a0d42175332d32
Adityawardhan Mishra <126398791+adityawardhanm@users.noreply.github.com>
2023-05-30 07:31:41 +0100

    Create Josephus_Problem.c

---
+++ C/Josephus_Problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int josephus(int n, int k){
+    if (n == 1){
+        return n;
+    }
+    else{
+        return (josephus(n - 1, k) + k - 1) % n + 1;
+    }
+}
+
+int main()5
+{
+    int n, k;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
+    printf("The safe place is %d\n", josephus(n, k));
+    return 0;
+}
